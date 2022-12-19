80426b607057b0fbe8116060fa8724ab680588ac
ox991 <oxkar991@gmail.com>
2022-12-18 22:23:17 -0500

    Correccion rutas con path *

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.use(express.json());
 //Rutas
 app.use("/api/auth", require("./routes/auth"));
 
+app.get('*', (req, res) => {
+  res.sendFile(__dirname +'/public/index.html')
+})
+
 //Escuchar peticiones
 app.listen(process.env.PORT, () => {
   console.log(`Servidor corriendo en el puerto ${process.env.PORT}`);
